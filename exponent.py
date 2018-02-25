bdab423f56c2cbcc90bfcd8b7bc5c531e307f48b
Tea Torovic <kai.nevermind@gmail.com>
2018-02-25 16:20:12 +0100

    Learned about using arithmetic in scripts. Finished exercises using input and replace methods.

---
+++ exponent.py
@@ -0,0 +1,6 @@
+user_input1 = input("Enter a base: ")
+user_input2 = input("Enter an exponent: ")
+
+print(user_input1, "to the power of", user_input2, "=", (float)(user_input1)**(float)(user_input2))
+
+print(f"{user_input1} to the power of {user_input2} = {(float)(user_input1)**(float)(user_input2)}")
