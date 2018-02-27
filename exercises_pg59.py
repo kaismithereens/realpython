a00a955850ced5aca6af51ac98738e8461e2675a
Tea Torovic <kai.nevermind@gmail.com>
2018-02-27 17:11:59 +0100

    Learned about loops, finished exercises on page 60.

---
+++ exercises_pg59.py
@@ -0,0 +1,16 @@
+for n in range(2, 11):
+	print(n)
+	
+num = 2
+while(num < 11):
+	print(num)
+	num = num + 1
+
+def doubles(number):
+	result = number
+	while(result < (number * 8)):
+		result = result * 2
+		print(result)
+
+doubles(2)
+	
