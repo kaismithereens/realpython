c9624658c28edd1c16fb7a10be6c0203e55d7a40
Tea Torovic <kai.nevermind@gmail.com>
2018-03-23 15:33:47 +0100

    Coin toss probability - keep tossing the coin until you see a different result in 10000 tries.

---
+++ coin_toss.py
@@ -0,0 +1,15 @@
+from random import randint
+
+flips = 0
+trials = 10000
+
+for i in range(1, trials):
+	if randint(0,1) == 0:
+		while randint(0,1)!= 1:
+			flips = flips + 1
+	elif randint(0,1) == 1:
+		while randint(0,1)!= 0:
+			flips = flips + 1
+
+print("The average number of flips was:",flips/trials)
+	
